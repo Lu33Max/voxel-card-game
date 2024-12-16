0d4f4842525d92465ddfe2b90cc54c59a6eed031
Nietod <nico-berndt@gmx.de>
2024-12-16 10:51:17 +0100

    created figure + their data

---
+++ Karxel/Assets/Scripts/Figure.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Figure : MonoBehaviour
+{
+    [SerializeField] FigureData figureData;
+
+    public void Move()
+    {
+
+    }
+
+    public void Attack()
+    {
+
+    }
+}
