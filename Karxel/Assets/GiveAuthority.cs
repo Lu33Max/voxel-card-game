a84a79b7309a71a28cbc493a2a0ab02372e297bd
Nietod <nico-berndt@gmx.de>
2024-12-18 19:08:15 +0100

    unit sync move?

---
+++ Karxel/Assets/GiveAuthority.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+public class GiveAuthority : NetworkBehaviour
+{
+    public NetworkIdentity cube;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.V))
+        {
+            Givee(cube);
+        }
+    }
+
+    [Command(requiresAuthority = false)]
+    public void Givee(NetworkIdentity cube)
+    {
+        base.OnStartLocalPlayer();
+
+
+
+        cube.GetComponent<NetworkIdentity>().AssignClientAuthority(GetComponent<NetworkIdentity>().connectionToClient);
+    }
+}
