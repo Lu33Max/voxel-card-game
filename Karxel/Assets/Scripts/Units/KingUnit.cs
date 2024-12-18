3c8cc5d8b6bf52574b9b3fc47b0034645c2011f5
Lu33Max <ernst_lucas@outlook.de>
2024-12-18 17:37:22 +0100

    Map gefixt + individuelles Movement Pattern

---
+++ Karxel/Assets/Scripts/Units/KingUnit.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class KingUnit : Unit
+{
+    public override List<MoveCommand> GetValidMoves(int movementRange)
+    {
+        var moves = new List<MoveCommand>();
+        var directions = new List<Vector2Int>
+        {
+            Vector2Int.down, Vector2Int.left, Vector2Int.right, Vector2Int.up, 
+        };
+
+        foreach (var direction in directions)
+        {
+            var path = new List<Vector2Int>();
+            
+            moves.Add(new MoveCommand { TargetPosition = TilePosition + direction, Path = path });
+        }
+        
+        return moves.Where(move => GridManager.Instance.IsMoveValid(move)).ToList();
+    }
+}
